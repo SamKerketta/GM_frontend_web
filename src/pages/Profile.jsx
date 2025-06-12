75555b2d8db37b72352ec9c082d455a86194fb8e
Anshu Kumar <anshu8581@gmail.com>
2025-06-12 13:57:13 +0530

    profile page design

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -21,74 +21,56 @@ const Profile = () => {
                   htmlFor="email"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Your email
+                  Email
                 </label>
                 <input
                   type="email"
                   id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@flowbite.com"
                   required
                 />
               </div>
               <div className="mb-5 col-span-6">
                 <label
-                  htmlFor="password"
+                  htmlFor="name"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Your Name
+                  Name
                 </label>
                 <input
-                  type="password"
-                  id="password"
+                  type="name"
+                  id="name"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required
                 />
               </div>
               <div className="mb-5 col-span-6">
                 <label
-                  htmlFor="password"
+                  htmlFor="mobile"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Your password
+                  Mobile
                 </label>
                 <input
-                  type="password"
-                  id="password"
+                  type="mobile"
+                  id="mobile"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   required
                 />
               </div>
               <div className="mb-5 col-span-6">
                 <label
-                  htmlFor="password"
+                  htmlFor="role"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Your password
+                  Role
                 </label>
                 <input
-                  type="password"
-                  id="password"
+                  type="role"
+                  id="role"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required
+                  disabled
                 />
-              </div>
-              <div className="flex items-start mb-5 col-span-12">
-                <div className="flex items-center h-5">
-                  <input
-                    id="remember"
-                    type="checkbox"
-                    defaultValue
-                    className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                    required
-                  />
-                </div>
-                <label
-                  htmlFor="remember"
-                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >
-                  Remember me
-                </label>
               </div>
 
               <div className="col-span-12">
@@ -103,6 +85,73 @@ const Profile = () => {
           </div>
 
           <hr className="col-span-12 my-10" />
+
+          <div className="col-span-4">
+            <div>
+              <h1 className="mb-4 text-xl font-extrabold tracking-tight leading-none text-gray-900 md:text-xl lg:text-xl dark:text-white">
+                Change Password
+              </h1>
+              <p className="mb-8 text-md font-normal text-gray-500 lg:text-lg dark:text-gray-400">
+                Update your password associated with your account.
+              </p>
+            </div>
+          </div>
+
+          <div className="col-span-8">
+            <form className="grid grid-cols-12 gap-4">
+              <div className="mb-5 col-span-12">
+                <label
+                  htmlFor="currentPassword"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Current Password
+                </label>
+                <input
+                  type="currentPassword"
+                  id="currentPassword"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              <div className="mb-5 col-span-12">
+                <label
+                  htmlFor="newPassword"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  New Password
+                </label>
+                <input
+                  type="newPassword"
+                  id="newPassword"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              <div className="mb-5 col-span-12">
+                <label
+                  htmlFor="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Confirm password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+
+              <div className="col-span-12">
+                <button
+                  type="submit"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
