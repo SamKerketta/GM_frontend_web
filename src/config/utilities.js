1af849603a8deb41da8bdc0913f4f7add8fc4127
anshukumar11062 <9939anshu@gmail.com>
2025-06-13 18:43:24 +0530

    ip changes

--- src/config/utilities.js
+++ src/config/utilities.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://13.234.76.248:8001/api';
+export const API_BASE_URL = 'http://65.0.73.240:8001/api';
 export const GYM_NAME = 'Gravity Unisex Fitness Club';
 export const GYM_EMAIL = 'info@gravityfintness.com';
 export const GYM_CONTACT = '+91 0123456789';
