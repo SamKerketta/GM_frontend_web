65f7e8157f27e61f46be3f1527d406232cc819d4
Anshu Kumar <anshu8581@gmail.com>
2025-07-05 12:37:41 +0530

    Add Plan bug fixed

--- src/pages/Plans.jsx
+++ src/pages/Plans.jsx
@@ -309,7 +309,10 @@ const Plans = () => {
 
         <Dialog
           open={openModal}
-          onClose={() => setOpenModal(false)}
+          onClose={() => {
+            setOpenModal(false);
+            setIsEditModal(false);
+          }}
           fullWidth
           maxWidth="sm" // Options: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | false
           slotProps={{
@@ -470,7 +473,10 @@ const Plans = () => {
           <DialogActions>
             <Button
               className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-              onClick={() => setOpenModal(false)}
+              onClick={() => {
+                setOpenModal(false);
+                setIsEditModal(false);
+              }}
             >
               Cancel
             </Button>
